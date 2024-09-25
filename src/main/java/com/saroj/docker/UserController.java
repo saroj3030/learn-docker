2937cb58dfe86463bf8a4b06a3f27d98425aa07e
saroj <saroj@localhost>
2024-09-25 20:44:52 +0530

    update

--- src/main/java/com/saroj/docker/UserController.java
+++ src/main/java/com/saroj/docker/UserController.java
@@ -3,6 +3,7 @@ package com.saroj.docker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class UserController {
 	public ResponseEntity<UserModel> getUser(@RequestBody UserModel model) {
 		UserModel userModel = userService.getUser(model);
 		return new ResponseEntity<>(userModel, HttpStatus.OK);
+	}
+	
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
 	}
 
 }
